9b28e281abd44d4901515eee9d773759a8d52be6
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-11-12 11:27:14 +0900

    deploy: update web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2040250158"
+    serviceWorkerVersion: "1637185692"
   }
 });
